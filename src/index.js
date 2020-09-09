197062e2880ec3af6cc4f484d2a389732c984a8a
Centon <nicolas.centon@gmail.com>
2020-09-09 08:40:52 +0200

    Lifting State up
    passe square value and onclick function from board class to square class with props. function onhandle don't been defined yet.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 class Square extends React.Component {
-    constructor(props){
-        super(props);
-        this.state ={
-            value: null,
-        };
-    }
+  
     render() {
       return (
-        <button className="square" onClick={() => this.setState({value: 'X'})}>
-          {this.state.value}
+        <button className="square" onClick={() => this.props.onClick()}>
+          {this.props.value}
         </button>
       );
     }
@@ -25,7 +20,11 @@ class Square extends React.Component {
       };
     }
     renderSquare(i) {
-      return <Square value={i} />;
+      return (
+        <Square value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+        />
+      );
     }
   
     render() {
